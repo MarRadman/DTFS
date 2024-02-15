ad54de16b0856e7a28789b9d3a6c077ba16fe51c
DrKhaliil <khaliil.m.hassan.2003@gmail.com>
2024-02-15 14:26:00 +0100

    Update HomeView.vue
    Registration

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,7 @@
     <p>Funkar det här nu</p>
     <h5>gr8</h5>
     <h3>david nilsson</h3>
+    <h3>Khaliil Hassan</h3>
     <h2>Nu skriver jag nått annat</h2>
     <h1>VÄLKOMMEN TILL DOWN TO FUCK SENIORS</h1>
     <h2>Rökbomb</h2>
