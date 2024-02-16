5b8123411ab55507f81ca84ac7407436ccf439a8
Martin <martinio@hotmail.se>
2024-02-16 14:09:26 +0100

    Ändrat lite med navmenu //Martin

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,27 +1,32 @@
 <template>
+  <h1>Välj din plattform för lärande</h1>
   <div>
-    <h1>Martin Simple is Key</h1>
-    <p>Funkar det här nu</p>
-    <h5>gr8</h5>
-    <h3>david nilsson</h3>
-    <h3>Khaliil Hassan</h3>
-    <h2>Nu skriver jag nått annat</h2>
-    <h1>VÄLKOMMEN TILL DOWN TO FUCK SENIORS</h1>
-    <h2>Rökbomb</h2>
-    <h3>rökbomb nr2</h3>
+    <b-button
+      title="Android"
+      img-src="#"
+      img-alt="Image"
+      style="max-width: 20rem"
+      class="mb-2"
+    >
+    </b-button>
   </div>
-
-  <!--Skapat en ny div // David Nilsson, innehåller text-->
   <div>
-    HEjsan alla kollegor
-    här kommer en helt ointressant text.
-    hejhejjhejhej
+    <b-button
+      title="Iphone"
+      img-src="#"
+      img-alt="image"
+      style="max-width: 20rem"
+      class="mb-2"
+    ></b-button>
   </div>
-  
+  <div>
+    <b-button
+      title="PC"
+      img-src="#"
+      img-alt="image"
+      style="max-width: 20rem"
+      class="mb-2"
+    ></b-button>
+  </div>
 </template>
-
-<script>
-
-</script>
-
-
+<script></script>
