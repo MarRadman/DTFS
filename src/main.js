1f3d64476a613e45892576b8adb10ccf5457bbfe
Martin <martinio@hotmail.se>
2024-02-16 10:50:46 +0100

    Bootstrap och en navmenu är implementerats //Martin

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/router";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue-next/dist/bootstrap-vue-next.css";
 
 const app = createApp(App);
 
