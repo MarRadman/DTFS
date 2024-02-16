e6cda0176785f85dd397885302974610d627254d
David Nilsson <David.Nilsson92@live.se>
2024-02-16 10:52:09 +0100

    Skapat en Login komponent & påbörjat den / David N

---
+++ src/components/LoginComponent.vue
@@ -0,0 +1,43 @@
+<template>
+
+<!--David Nilsson kör Login formulär-->
+<div>
+  <form class="form-container">
+   <h2>Login</h2>
+   <input type="text" v-model="username">
+   <input type="text" v-model="password">
+   <input type="checkbox">
+   <p>Forgot password?</p>
+   <button>Login</button>
+   <p>Don't have an account?</p>
+   <p>Register</p>
+ </form>
+</div>
+
+<div>
+    <form class="form-container">
+      <h2>Create Account</h2> 
+      <input type="text" v-model="username">
+      <input type="text" v-model="password"> 
+      <input type="text" v-mdel="password">
+      <button>Sign up</button>
+      <p>Already have an account?</p>
+    </form>
+</div>
+
+
+</template>
+
+<script>
+
+export default{
+
+    data(){
+        return{
+        username: "",
+        password: ""
+        }
+    }
+}
+
+</script>
