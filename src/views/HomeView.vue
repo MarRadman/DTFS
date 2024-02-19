10b21a374da87b7956a92d4040a992b05707fbc6
david arvidsson <david.arvidsson.da@egmail.com>
2024-02-19 09:49:28 +0100

    fixat lite davida

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,32 +1,21 @@
 <template>
   <h1>Välj din plattform för lärande</h1>
   <div>
-    <b-button
-      title="Android"
-      img-src="#"
-      img-alt="Image"
-      style="max-width: 20rem"
-      class="mb-2"
-    >
-    </b-button>
-  </div>
-  <div>
-    <b-button
-      title="Iphone"
-      img-src="#"
-      img-alt="image"
-      style="max-width: 20rem"
-      class="mb-2"
-    ></b-button>
-  </div>
-  <div>
-    <b-button
-      title="PC"
-      img-src="#"
-      img-alt="image"
-      style="max-width: 20rem"
-      class="mb-2"
-    ></b-button>
-  </div>
+                                      <a href="/DTFS/src/views/SecondView.vue">
+                                      <img src="/DTFS/src/assets/Andriod.jpg" alt="android" style="max-width: 20rem;" class="mb-2">
+                      </a>
+                                </div>
+                                <div>
+                               <a href="">
+                                <img src="" alt="">
+                               </a>
+                                </div>
+                                <div>
+                       <a href="">
+                        <img src="" alt="">
+                       </a>
+            </div>
 </template>
-<script></script>
+<script>
+
+</script>
