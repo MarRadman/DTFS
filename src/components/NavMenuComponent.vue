25226252fc44694e8534043def1ac5f8ecf0766e
Martin <martinio@hotmail.se>
2024-02-20 14:45:41 +0100

    Fixat //Martin

--- src/components/NavMenuComponent.vue
+++ src/components/NavMenuComponent.vue
@@ -22,6 +22,9 @@
           >
         </li> -->
         <li class="nav-item">
+          <router-link to="/login" class="nav-link">Login</router-link>
+        </li>
+        <li class="nav-item">
           <router-link to="/member" class="nav-link">Member</router-link>
         </li>
         <li class="nav-item">
