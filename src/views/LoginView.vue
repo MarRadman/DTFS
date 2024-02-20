01d2830313227bfd6cbc65f9d5b715850a82ae91
David Nilsson <David.Nilsson92@live.se>
2024-02-20 13:19:10 +0100

    Påbörjat formulär, ett för login och ett för registering. Ej skapat någon funktionalitet ännu. Det blir nästa steg // David Nilsson

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,49 +1,153 @@
 <template>
-  <!--David Nilsson kör Login formulär-->
-
-  <!--Jag vill även lägga till ikoner för username & password-->
-
-  <div>
-    <form class="form-container">
-      <h2>Login</h2>
-      <input type="text" v-model="username" />
-      <input type="text" v-model="password" />
-      <input type="checkbox" />
-      <p>Forgot password?</p>
-      <button>Login</button>
-      <p>Don't have an account?</p>
-      <p>Register</p>
-    </form>
+  
+  <h1>Test Formulär </h1>
+  
+  <div class="form">
+   <h2>LOGIN</h2>
+   <form>
+      <div class="form-container">
+       <label for="username">USERNAME</label>
+       <input type="text"  required>
+      </div>
+  
+      <div class="form-container">
+      <label for="password">PASSWORD</label>
+      <input type="password"  required>
+      </div>
+  
+      <div>  <!--Kommer ej göra nånting, ska mest föreställa "riktigt" formulär-->
+       <input type="checkbox">
+       <label>Remember me</label>
+      </div>
+  
+      <button class="login-btn">LOGIN</button> <!--Denna knapp ska ta oss till HomeView.-->
+      <p class="register">New here? Please <a href="#" class="register-link"><strong>Register</strong></a></p>
+      
+   </form>
+  
   </div>
-
-  <!--Denna ska endast visas om användaren inte har ett konto.-->
-  <!--Login formuläret försvinner då, och när användaren tryckt på Login igen, då kommer man tillbaka till Login sidans-->
-  <!--Username & Password ska ha villkor. Dvs x antal bokstäver, stora/små bokstäver-->
-  <!--Om villkoren ej uppfylls, ska det bli rödmarkerat. Grönt om allt uppfylls-->
-
-  <div>
-    <form class="form-container">
-      <h2>Create Account</h2>
-      <input type="text" v-model="username" />
-      <input type="text" v-model="password" />
-      <input type="text" v-model="password" />
-      <button>Sign up</button>
-      <p>Already have an account? Login</p>
-    </form>
+  
+  <!--CREATE ACCOUNT ska endast dyka upp när använder trycker på 'Register'-->
+  <!--När användaren fyllt i sin information och tryckt på SIGN UP. Ska använderaren skickas tillbaka till LOGIN-->
+  
+  <div class="form">
+      <h2>CREATE ACCOUNT</h2>
+      <form>
+          <div class="form-container">
+              <label for="username">CHOOSE A USERNAME</label>
+              <input type="text"  v-model="username" required>
+          </div>
+  
+          <div class="form-container">
+              <label for="password">CHOOSE A PASSWORD</label>
+              <input type="text" v-model="password" required>
+          </div>
+          <div class="form-container">
+              <label for="password">TYPE PASSWORD AGAIN</label>
+              <input type="password" required>
+          </div>
+  
+          <div class="terms">
+              <input id="check" type="checkbox" v-model="terms">
+              <p>ACCEPT TERMS</p>
+          </div>
+          
+          <button class="login-btn">SIGN UP</button>
+  
+  
+          <!--Kollar av om v-model fungerar-->
+          <p> Username: {{ username }}</p>
+          <p>Password: {{ password }}</p>
+          <p>Kolla terms: {{ terms }}</p>
+        
+      </form>
   </div>
-</template>
-
-<script>
-export default {
-  data() {
-    return {
-      username: "",
-      password: "",
-    };
-  },
-  // Jag kommer använda computed() för att kolla igenom villkoren.
-  // En v-if på Register formuläret.
-};
-</script>
-
-<style></style>
+  
+  </template>
+  
+  <script>
+  
+   export default{
+      data(){
+          return{    // Dessa variabler ska sedan användas som villkor i formuläret. 
+              username: "",
+              password: "",
+              terms: false
+          }
+      },
+      methods: { // Härnäst kommer jag skapa funktionalitet i formuläret. Det kommer bli i CREATE ACCOUNT. 
+  
+      }
+  }
+  
+  </script>
+  
+  <style>
+  
+  .form{
+  
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      max-width: 360px;
+      min-height: 550px;
+      margin: 30px auto;
+      padding: 40px;
+      border: solid black 1px;
+      border-radius: 10px;
+      background-color: #F8FAF4;
+      text-align: center;
+  }
+  label{
+      text-align: center;
+      color: #aaa;    
+      display: inline-block;
+      margin: 20px 0 10px;
+      font-size: 1rem;
+      letter-spacing: 1;
+  }
+  .form-container{
+      margin-bottom: 15px;
+  
+  }
+  .login-btn{
+      width: 100%;
+      height: 40px;
+      background-color: #A0BC59;
+      color: white;
+      border: solid black 1px;
+      border-radius: 10px;
+      margin-top: 10px;
+  }
+  
+  input{
+      background-color: seashell;
+      color: black;
+      display: block;
+      padding: 5px 3px;
+      width: 100%;
+      
+  }
+  
+  .register{
+      margin-top: 40px;
+  }
+  .register-link{
+    text-decoration: none;
+  }
+  
+  .terms{
+      display: flex;
+      justify-content: space-between;
+  }
+  
+  .terms p {
+   
+      margin-left: 20px;
+  }
+  #check{
+      max-width: 15px;
+      height: auto;
+  }
+  </style>
