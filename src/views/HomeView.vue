217bbaa0a9f84838125e05fe06b06a8b48788299
david arvidsson <david.arvidsson.da@egmail.com>
2024-02-20 13:05:10 +0100

    fix Davida

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,23 @@
 <template>
   <h1>Välj din plattform för lärande</h1>
-                <div>
-                <router-link to="SecView">
-                  <img src="../assets/Andriod.jpg" alt="android" style="max-width: 20rem" class="mb-2">
-                </router-link>
+              <div>
+              <router-link to="SecView">
+              <img src="../assets/Andriod.jpg" alt="android" style="max-width: 20rem" class="mb-2">
+              </router-link>
               </div>
               <div>
-                  <a href="#">
-                  <img src="../assets/macbook.jpg" alt="Mac" style="max-width: 20rem;" class="mb-2">
-                  </a>
+              <router-link to="#">
+              <img src="../assets/macbook.jpg" alt="Mac" style="max-width: 20rem;" class="mb-2">
+              </router-link>
               </div>
               <div>
-                  <a href="#">
-                  <img src="../assets/Windows2.jpg" alt="Windows" style="max-width: 20rem;" class="mb-2">
-              </
-a>
+              <router-link to="#">
+              <img src="../assets/Windows2.jpg" alt="Windo
+ws" style="max-width: 20re
+m;" class=
+"mb-2">
+        <
+/router-link>>
   </div>
 </template>
 
