74f6488fc988e71f4415c63354ecce573ccb3f07
david arvidsson <david.arvidsson.da@egmail.com>
2024-02-20 11:39:55 +0100

    strunt

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,35 +1,25 @@
 <template>
   <h1>Välj din plattform för lärande</h1>
-  <div>
-    <a href="./SecondView.vue">
-      <img
-        src="../assets/Andriod.jpg"
-        alt="android"
-        style="max-width: 20rem"
-        class="mb-2"
-      />
-    </a>
-  </div>
-  <div>
-    <a href="#">
-      <img
-        src="../assets/macbook.jpg"
-        alt="Mac"
-        style="max-width: 20rem"
-        class="mb-2"
-      />
-    </a>
-  </div>
-  <div>
-    <a href="#">
-      <img
-        src="../assets/Windows2.jpg"
-        alt="Windows"
-        style="max-width: 20rem"
-        class="mb-2"
-      />
-    </a>
+                <div>
+                <router-link to="SecView">
+                  <img src="../assets/Andriod.jpg" alt="android" style="max-width: 20rem" class="mb-2">
+                </router-link>
+              </div>
+              <div>
+                  <a href="#">
+                  <img src="../assets/macbook.jpg" alt="Mac" style="max-width: 20rem;" class="mb-2">
+                  </a>
+              </div>
+              <div>
+                  <a href="#">
+                  <img src="../assets/Windows2.jpg" alt="Windows" style="max-width: 20rem;" class="mb-2">
+              </
+a>
   </div>
 </template>
 
-<script></script>
+<script>
+
+</script>
+
+
