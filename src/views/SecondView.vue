388804d4a112344485c25a0cc9dd1eb903c2a168
Martin <martinio@hotmail.se>
2024-02-20 15:08:09 +0100

    Kommenterat ut methoden i SecView //Martin

--- src/views/SecondView.vue
+++ src/views/SecondView.vue
@@ -1,6 +1,5 @@
-<<<<<<< HEAD:src/components/SecondViewComponent.vue
 <template>
-                  <!--Jag använde först vanliga knappar och kopplade ihop med bild osv manuellt men såg sedan att någon hade gjort ett exempel med b-card
+  <!--Jag använde först vanliga knappar och kopplade ihop med bild osv manuellt men såg sedan att någon hade gjort ett exempel med b-card
                   så jag kollade upp hur man använder det och bestämde mig för att göra på detta sättet istället. Känns smidigare. Har inte testat bootstrap
                   så mycket än så skall skapa ett separat dokument på datorn så jag kan se hur det ser ut och style'a så det ser rätt ut då.
                   Majoriteten av koden skriven 02-20-2024 //Jonathan-->
@@ -11,14 +10,17 @@
       img-alt=""
       img-top
       tag=""
-      style="max-width: 20rem;"
+      style="max-width: 20rem"
       class="mb-2"
     >
-    <b-card-text>
-      Knapp 1 som skall leda till en annan komponent på sidan.
-    </b-card-text>
+      <b-card-text>
+        Knapp 1 som skall leda till en annan komponent på sidan.
+      </b-card-text>
 
-    <b-button @click="navigateToNextPage('')" variant="primary">Klicka här</b-button> <!--Komponentnamn i paratesen vid @click så kommer man vidare till nästa komponent-->
+      <b-button @click="navigateToNextPage('')" variant="primary"
+        >Klicka här</b-button
+      >
+      <!--Komponentnamn i paratesen vid @click så kommer man vidare till nästa komponent-->
     </b-card>
   </div>
 
@@ -29,14 +31,19 @@
       img-alt=""
       img-top
       tag=""
-      style="max-width: 20rem;"
+      style="max-width: 20rem"
       class="mb-2"
     >
-    <b-card-text>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor consequatur, similique, quam doloribus adipisci odio illo facilis accusantium voluptates ex ducimus maiores et sunt totam dignissimos natus dicta itaque placeat?
-    </b-card-text>
+      <b-card-text>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
+        consequatur, similique, quam doloribus adipisci odio illo facilis
+        accusantium voluptates ex ducimus maiores et sunt totam dignissimos
+        natus dicta itaque placeat?
+      </b-card-text>
 
-    <b-button @click="navigateToNextPage('')" variant="primary">Klicka här</b-button>
+      <b-button @click="navigateToNextPage('')" variant="primary"
+        >Klicka här</b-button
+      >
     </b-card>
   </div>
 
@@ -47,32 +54,33 @@
       img-alt=""
       img-top
       tag=""
-      style="max-width: 20rem;"
+      style="max-width: 20rem"
       class="mb-2"
     >
-    <b-card-text>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos ipsum odio quidem animi ut quos fugiat, tempora sint quaerat suscipit expedita, iusto necessitatibus velit consectetur ea voluptas dolore sapiente. Voluptates!
-    </b-card-text>
+      <b-card-text>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos ipsum odio
+        quidem animi ut quos fugiat, tempora sint quaerat suscipit expedita,
+        iusto necessitatibus velit consectetur ea voluptas dolore sapiente.
+        Voluptates!
+      </b-card-text>
 
-    <b-button @click="navigateToNextPage('')" variant="primary">Klicka här</b-button>
+      <b-button @click="navigateToNextPage('')" variant="primary"
+        >Klicka här</b-button
+      >
     </b-card>
   </div>
-
 </template>
-=======
-<template>Hej</template>
->>>>>>> db5cf53217515ade903c9599b85306e26d5ba4fb:src/views/SecondView.vue
 
 <script>
 export default {
-  name: 'SeconViewComponent',
+  name: "SeconViewComponent",
 
-  methods: {
+  // methods: {
 
-    navigateToNextPage() { //Metod för att knappen ska navigera till nästa komponent, komponentnamn i parantesen
-      this.$router.push({name: }) //komponentnamn i name
-    }
+  //   navigateToNextPage() { //Metod för att knappen ska navigera till nästa komponent, komponentnamn i parantesen
+  //     this.$router.push({name: }) //komponentnamn i name
+  //   }
 
-  }
+  // }
 };
 </script>
