666e3ae6e488086ad7115d8bfe7fea6a0be6738b
david arvidsson <david.arvidsson.da@egmail.com>
2024-02-20 14:11:55 +0100

    fix

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,29 +1,29 @@
 <template>
   <h1>Välj din plattform för lärande</h1>
-                <div>
-                <router-link to="SecView">
-                <img src="../assets/Andriod.jpg" alt="android" style="max-width: 20rem" class="mb-2">
-                </router-link>
-                </div>
-                <div>
-                  <!--
-      <router-link to="#">-->
-                <img src="../assets/macbook.jpg" alt="Mac" style="max-width: 20rem;" class="mb-2">
-                <!--
-      </router-link>-->
-                </div>
-                <div>
-                  <!--
-      <router-link to="#">-->
-              <img src="../assets/Windows2.jpg" alt="Windows" style="max-width: 20rem;" class="mb-2">
-        <!--
-      </route
-r-link>-->
+        <div>
+          <b-card>
+            <router-link to="SecView">
+              <img src="../assets/Andriod.jpg" alt="android" style="max-width: 20rem" class="mb-2" />
+              Andriod
+            </router-link>
+          </b-card>
+        </div>
+        <div>
+          <b-card>
+            <!--<router-link to="#">-->
+            <img src="../assets/macbook.jpg" alt="Mac" style="max-width: 20rem" class="mb-2" />
+            <!--</router-link>-->
+          </b-card>
+        </div>
+        <div>
+          <b-card>
+            <!--<router-link to="#">-->
+            <img src="../assets/Windows2.jpg" alt="Windows" st yle="max-width: 20
+rem" class="mb-2" />
+        <!--</router-link>-->
+      </b
+-card>
   </div>
 </template>
 
-<script>
-
-</script>
-
-
+<script></script>
