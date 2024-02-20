e79f72e074a5fbe230116ae4951f8b0e8e92b2c5
david arvidsson <david.arvidsson.da@egmail.com>
2024-02-20 10:02:39 +0100

    ändrat lite

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,19 @@
 <template>
   <h1>Välj din plattform för lärande</h1>
-                                      <div>
-                                        <a href="./SecondView.vue">
-                                          <img src="../assets/Andriod.jpg" alt="android" style="max-width: 20rem" class="mb-2">
-                                        </a>
-                                      </div>
-                                      <div>
-                                              <a href="#">
-                                                <img src="../assets/macbook.jpg" alt="Mac" style="max-width: 20rem;" class="mb-2">
-                                              </a>
-                                        </div>
-                                        <div>
-                              <a href="#">
-                                <img src="../assets/Windows2.jpg" alt="Windows" style="max-width: 20rem;" class="mb-2">
-                  </a>
+      <div>
+          <a href="./SecondView.vue">
+          <img src="../assets/Andriod.jpg" alt="android" style="max-width: 20rem" class="mb-2">
+          </a>
+      </div>
+      <div>
+          <a href="#">
+          <img src="../assets/macbook.jpg" alt="Mac" style="max-width: 20rem;" class="mb-2">
+          </a>
+      </div>
+      <div>
+          <a href="#">
+          <img src="../assets/Windows2.jpg" alt="Windows" style="max-width: 20rem;" class="mb-2">
+          </a>
   </div>
 </template>
 
