ec29b1df927b54cd20b43ed3654d29b541e22cbd
david arvidsson <david.arvidsson.da@egmail.com>
2024-02-21 12:33:38 +0100

    förmodligen inget

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,26 @@
 <template>
   <h1>Välj din plattform för lärande</h1>
-        <div>
-          <b-card>
-            <router-link to="SecView">
-              <img src="../assets/Andriod.jpg" alt="android" style="max-width: 20rem" class="mb-2" />
-              Andriod
-            </router-link>
-          </b-card>
-        </div>
-        <div>
-          <b-card>
-            <!--<router-link to="#">-->
-            <img src="../assets/macbook.jpg" alt="Mac" style="max-width: 20rem" class="mb-2" />
-            <!--</router-link>-->
-          </b-card>
-        </div>
-        <div>
-          <b-card>
-            <!--<router-link to="#">-->
-            <img src="../assets/Windows2.jpg" alt="Windows" st yle="max-width: 20
-rem" class="mb-2" />
-        <!--</router-link>-->
-      </b
--card>
+      <div>
+        <b-card>
+          <router-link to="SecView">
+            <img src="../assets/Andriod.jpg" alt="android" style="max-width: 20rem" class="mb-2" />
+          </router-link>
+        </b-card>
+      </div>
+      <div>
+        <b-card>
+          <!--<router-link to="#">-->
+          <img src="../assets/macbook.jpg" alt="Mac" style="max-width: 20rem" class="mb-2" />
+          <!--</router-link>-->
+        </b-card>
+      </div>
+      <div>
+        <b-card>
+          <!--<router-link to="#">-->
+        <img src="../assets/Windows2.jpg" alt="Windows" style="max-width: 20rem" class="mb-2" />
+        <!--</router-l
+ink>-->
+    </b-card>
   </div>
 </template>
 
