095856e266af2d5833df43f35ebebba3db640de6
Martin <martinio@hotmail.se>
2024-02-22 11:38:49 +0100

    Fixat i SecView //Martin

--- src/views/SecondView.vue
+++ src/views/SecondView.vue
@@ -3,66 +3,65 @@
                   så jag kollade upp hur man använder det och bestämde mig för att göra på detta sättet istället. Känns smidigare. Har inte testat bootstrap
                   så mycket än så skall skapa ett separat dokument på datorn så jag kan se hur det ser ut och style'a så det ser rätt ut då.
                   Majoriteten av koden skriven 02-20-2024 //Jonathan-->
-<div class="container">
-  <div id="conOne">
-    <b-card
-      id="cardOne">
-    <img
-      style="max-width: 15rem; max-height: 10rem;"
-      src="../assets/Andriod.jpg"
-      alt="Android">
-      <b-card-text>
-        <h1>Step by Step</h1>
+  <div class="container">
+    <div id="conOne">
+      <b-card id="cardOne">
+        <img
+          style="max-width: 15rem; max-height: 10rem"
+          src="../assets/Andriod.jpg"
+          alt="Android"
+        />
+        <b-card-text>
+          <h1>Step by Step</h1>
 
-        <p>Your guide of learning basic IT</p>
-      </b-card-text>
-      <b-button
-      style="background-color: blue;"
-      router-link to="/android"
-        >Start</b-button>
-    </b-card>
+          <p>Your guide of learning basic IT</p>
+        </b-card-text>
+        <b-button style="background-color: blue" router-link to="/android"
+          >Start</b-button
+        >
+      </b-card>
+    </div>
+
+    <div id="conTwo">
+      <b-card id="cardTwo">
+        <img
+          style="max-width: 15rem; max-height: 10rem"
+          src="../assets/Quiz.jpg"
+          alt="Quiz"
+        />
+        <b-card-text>
+          <h1>Quiz</h1>
+
+          <p>Test your knowledge in a quiz!</p>
+        </b-card-text>
+
+        <b-button style="background-color: blue" router-link to="/android"
+          >Start</b-button
+        >
+      </b-card>
+    </div>
+
+    <div id="conThree">
+      <b-card id="cardThree">
+        <img
+          style="max-width: 15rem; max-height: 10rem"
+          src="../assets/Community.jpg"
+          alt="Community"
+        />
+        <b-card-text>
+          <h1>Community</h1>
+
+          <p>
+            Join the community with others and share your thoughts and skills
+          </p>
+        </b-card-text>
+
+        <b-button style="background-color: blue" router-link to="/android"
+          >Start</b-button
+        >
+      </b-card>
+    </div>
   </div>
-
-  <div id="conTwo">
-    <b-card
-      id="cardTwo">
-    <img
-      style="max-width: 15rem; max-height: 10rem;"
-      src="../assets/Quiz.jpg"
-      alt="Quiz">
-      <b-card-text>
-        <h1>Quiz</h1>
-
-        <p>Test your knowledge in a quiz!</p>
-      </b-card-text>
-
-      <b-button
-      style="background-color: blue;"
-      router-link to="/android"
-        >Start</b-button>
-    </b-card>
-  </div>
-
-  <div id="conThree">
-    <b-card
-      id="cardThree">
-    <img
-      style="max-width: 15rem; max-height: 10rem;"
-      src="../assets/Community.jpg"
-      alt="Community">
-      <b-card-text>
-        <h1>Community</h1>
-
-        <p>Join the community with others and share your thoughts and skills</p>
-      </b-card-text>
-
-      <b-button
-      style="background-color: blue;"
-      router-link to="/android"
-        >Start</b-button>
-    </b-card>
-  </div>
-
 </template>
 
 <script>
@@ -80,41 +79,37 @@ export default {
 </script>
 
 <style>
-
 @media screen and (max-width: 768px) {
-.container {
-  display: flex;
-  flex-direction: column;
+  .container {
+    display: flex;
+    flex-direction: column;
+  }
 
+  #conOne {
+    padding: 1rem;
+  }
+
+  #conTwo {
+    padding: 1rem;
+  }
+
+  #conThree {
+    padding: 1rem;
+  }
+
+  #cardOne {
+    width: 20rem;
+    height: 23rem;
+  }
+
+  #cardTwo {
+    width: 20rem;
+    height: 23rem;
+  }
+
+  #cardThree {
+    width: 20rem;
+    height: 23rem;
+  }
 }
-
-#conOne {
-  padding: 1rem;
-}
-
-#conTwo {
-  padding: 1rem;
-}
-
-#conThree {
-  padding: 1rem;
-}
-
-#cardOne {
-  width: 20rem;
-  height: 23rem;
-}
-
-#cardTwo {
-  width: 20rem;
-  height: 23rem;
-}
-
-#cardThree {
-  width: 20rem;
-  height: 23rem;
-}
-}
-
-
 </style>
