d535a379f4969a9231cb6bdbaea7ead714ed7822
David Nilsson <David.Nilsson92@live.se>
2024-02-22 14:05:36 +0100

    oklart vad som händer/ david n

---
+++ src/views/Firstview.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Välj din plattform för lärande</h1>
+    <div>
+        <b-button title="Android" img-src="#" img-alt="Image" style="max-width: 20rem;" class="mb-2">
+        </b-button>
+    </div>
+    <div>
+        <b-button title="Iphone" img-src="#" img-alt="image" style="max-width:20rem;" class="mb-2"></b-button>
+    </div>
+    <div>
+        <b-button title="PC" img-src="#" img-alt="image" style="max-width: 20rem;" class="mb-2"></b-button>
+    </div>
+</template>
