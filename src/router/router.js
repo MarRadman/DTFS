145d360bdff7c19749ca6a3853289f7a320f34cc
DrKhaliil <khaliil.m.hassan.2003@gmail.com>
2024-02-22 11:07:59 +0100

    Khaliil added images, fixed andriodsel

--- src/router/router.js
+++ src/router/router.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/HomeView.vue";
 import Login from "../views/LoginView.vue";
 import SecView from "../views/SecondView.vue";
-import Android from "../views/androidselectionview.vue";
+import Android from "../views/AndroidselectionView.vue";
 import About from "../views/AboutView.vue";
 import Member from "../views/MemberView.vue";
 import Learning from "../views/LearningstepsView.vue";
