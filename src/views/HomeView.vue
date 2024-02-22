fc0b71ecb11bdeba64247cb8eff57ed820041b2b
david arvidsson <david.arvidsson.da@egmail.com>
2024-02-22 21:42:08 +0100

    nu ger jag mig

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,36 +1,35 @@
 <template>
-        <h1 style="font-weight: bold;">Välj din plattform för lärande</h1>
-        <div>
-          <b-card>
-            <router-link to="SecView">
-              <img src="../assets/Andriod.jpg" alt="android" style="max-width:20rem" class="mb-2" />
-              <span
-                style="position: absolute; bottom: 0; right: 40%; transform: translateY(-50%); color: white; background-color: rgba(0, 0, 0, 0.5); padding: 5px; font-size: 20px; font-weight: bold;">Android</span>
-            </router-link>
-          </b-card>
-        </div>
-        <div>
-          <b-card>
-            <!--<router-link to="#">--> <!--uppdateras under sprint två-->
-            <img src="../assets/macbook.jpg" alt="Mac" style="max-width: 20rem" class="mb-2" />
+      <h1 style="font-weight: bold;">Välj din plattform för lärande</h1>
+      <div>
+        <b-card>
+          <router-link to="SecView">
+            <img src="../assets/Andriod.jpg" alt="android" style="max-width:20rem" class="mb-2" />
             <span
-              style="position: absolute; bottom: 0; right: 40%; transform: translateY(-50%); color: white; background-color: rgba(0, 0, 0, 0.5); padding: 5px; font-size: 20px; font-weight: bold;">Apple</span>
-            <!--</router-link>--> <!--uppdateras under sprint två-->
-          </b-card>
-        </div>
-        <div>
-          <b-card>
-            <!--<router-link to="#">--> <!--uppdateras under sprint två-->
-            <img src="../assets/Windows2.jpg" alt="Windows" style="max-width:20rem" class="mb-2" />
-            <span style="position: absolute; bottom: 0; right: 40%; transform: translateY(-50%); color: white;background-color: rgba(0, 0, 0, 0
-.5); padding: 5px; font-size: 20px; font-wei
-ght: bold;">Windows</
-span>
-          <!--<
-/router-link>--> <!--uppdatera
-s under sprint två-->
+              style="position: absolute; bottom: 0; right: 40%; transform: translateY(-50%); color: white; background-color: rgba(0, 0, 0, 0.5); padding: 5px; font-size: 20px; font-weight: bold;">Android</span>
+          </router-link>
+        </b-card>
+      </div>
+      <div>
+        <b-card>
+          <!--<router-link to="#">--> <!--uppdateras under sprint två-->
+          <img src="../assets/macbook.jpg" alt="Mac" style="max-width: 20rem" class="mb-2" />
+          <span
+            style="position: absolute; bottom: 0; right: 40%; transform: translateY(-50%); color: white; background-color: rgba(0, 0, 0, 0.5); padding: 5px; font-size: 20px; font-weight: bold;">Apple</span>
+          <!--</router-link>--> <!--uppdateras under sprint två-->
+        </b-card>
+      </div>
+      <div>
+        <b-card>
+          <!--<router-link to="#">--> <!--uppdateras under sprint två-->
+          <img src="../assets/Windows2.jpg" alt="Windows" style="max-width:20rem" class="mb-2" />
+          <span
+            style="position: absolute; bottom: 0; right: 40%; transform: translateY(-50%); color: white;background-color: rgba(0, 0, 0, 0.5); padding: 5px; font-size: 20px; font-weight: bold;">Windows</span>
+          <!--</router-link>--> <!--uppdateras under sprint två-->
     </b-card>
   </div>
 </template>
 
-<script></script>
+<script>
+</script>
+
+
