50c69fd6056acc7bde9a885f61a9ebfa46862ec5
david arvidsson <david.arvidsson.da@egmail.com>
2024-02-26 10:53:12 +0100

    first view/vue färdig

--- src/router/router.js
+++ src/router/router.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/HomeView.vue";
 import Login from "../views/LoginView.vue";
 import SecView from "../views/SecondView.vue";
+import Mac from "../views/SecondViewapple.vue";
+import Windows from "../views/SecondViewPC.vue";
 import Android from "../views/AndroidselectionView.vue";
 import About from "../views/AboutView.vue";
 import Member from "../views/MemberView.vue";
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,35 +1,55 @@
 <template>
-      <h1 style="font-weight: bold;">Välj din plattform för lärande</h1>
-      <div>
-        <b-card>
-          <router-link to="SecView">
-            <img src="../assets/Andriod.jpg" alt="android" style="max-width:20rem" class="mb-2" />
-            <span
-              style="position: absolute; bottom: 0; right: 40%; transform: translateY(-50%); color: white; background-color: rgba(0, 0, 0, 0.5); padding: 5px; font-size: 20px; font-weight: bold;">Android</span>
+  <h1 style="font-weight: bold">Välj din plattform för lärande</h1>
+  <div class="d-flex flex-column flex-md-row">
+    <b-card class="mr-md-2 mb-md-0 mb-2">
+      <router-link to="SecView">
+        <img src="../assets/Andriod.jpg" alt="android" style="max-width: 20rem" class="mb-2" />
+        <span style="
+            position: absolute;
+            bottom: 0;
+            right: 40%;
+            transform: translateY(-50%);
+            color: white;
+            background-color: rgba(0, 0, 0, 0.5);
+            padding: 5px;
+            font-size: 20px;
+            font-weight: bold;
+          ">Android</span>
+      </router-link>
+    </b-card>
+    <b-card class="mr-md-2 mb-md-0 mb-2">
+      <router-link to="Mac">
+      <img src="../assets/macbook.jpg" alt="Mac" style="max-width: 20rem" class="mb-2" />
+      <span style="
+          position: absolute;
+          bottom: 0;
+          right: 40%;
+          transform: translateY(-50%);
+          color: white;
+          background-color: rgba(0, 0, 0, 0.5);
+          padding: 5px;
+          font-size: 20px;
+          font-weight: bold;
+        ">Apple</span>
+        </router-link>
+    </b-card>
+    <b-card>
+      <router-link to="Windows">
+      <img src="../assets/Windows2.jpg" alt="Windows" style="max-width: 20rem" class="mb-2" />
+      <span style="
+          position: absolute;
+          bottom: 0;
+          right: 40%;
+          transform: translateY(-50%);
+          color: white;
+          background-color: rgba(0, 0, 0, 0.5);
+          padding: 5px;
+          font-size: 20p x;
+          font-weight: bold;
+          ">Windows</span>
           </router-link>
-        </b-card>
-      </div>
-      <div>
-        <b-card>
-          <!--<router-link to="#">--> <!--uppdateras under sprint två-->
-          <img src="../assets/macbook.jpg" alt="Mac" style="max-width: 20rem" class="mb-2" />
-          <span
-            style="position: absolute; bottom: 0; right: 40%; transform: translateY(-50%); color: white; background-color: rgba(0, 0, 0, 0.5); padding: 5px; font-size: 20px; font-weight: bold;">Apple</span>
-          <!--</router-link>--> <!--uppdateras under sprint två-->
-        </b-card>
-      </div>
-      <div>
-        <b-card>
-          <!--<router-link to="#">--> <!--uppdateras under sprint två-->
-          <img src="../assets/Windows2.jpg" alt="Windows" style="max-width:20rem" class="mb-2" />
-          <span
-            style="position: absolute; bottom: 0; right: 40%; transform: translateY(-50%); color: white;background-color: rgba(0, 0, 0, 0.5); padding: 5px; font-size: 20px; font-weight: bold;">Windows</span>
-          <!--</router-link>--> <!--uppdateras under sprint två-->
     </b-card>
   </div>
 </template>
 
-<script>
-</script>
-
-
+<script></script>
