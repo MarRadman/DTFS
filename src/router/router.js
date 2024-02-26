6970daae40642dab9881a8994c327ee3f4335cdf
MarRadman <martinio@hotmail.se>
2024-02-26 12:23:40 +0100

    Ny mapp Android tillagd. Mappat om //Martin

--- src/router/router.js
+++ src/router/router.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/HomeView.vue";
 import Login from "../views/LoginView.vue";
-import SecView from "../views/SecondView.vue";
-import Mac from "../views/SecondViewapple.vue";
+import SecView from "../views/Android/SecondViewAndroid.vue";
+import Mac from "../views/SecondViewApple.vue";
 import Windows from "../views/SecondViewPC.vue";
 import Android from "../views/AndroidselectionView.vue";
 import About from "../views/AboutView.vue";
 import Member from "../views/MemberView.vue";
-import Learning from "../views/LearningstepsView.vue";
+import Learning from "../views/Android/Stepbystep/LearningstepsView.vue";
 
 const routes = [
   {
