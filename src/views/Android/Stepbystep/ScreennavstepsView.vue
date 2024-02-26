719f58485ec55dfbccba0e9b1070985e12d99903
david arvidsson <david.arvidsson.da@egmail.com>
2024-02-26 22:33:53 +0100

    smått

---
+++ src/views/Android/Stepbystep/ScreennavstepsView.vue
@@ -0,0 +1,66 @@
+<template>
+<div>
+  <div class="mt-4">
+    <h4>Navigation buttons at the bottom.</h4>
+    <b-card img-src="https://placekitten.com/300/300" img-alt="Card image" img-left class="mb-3">
+      <b-card-text>
+        At the bottom of the screen, usually in a row, you'll find the navigation buttons. These can be icons such as a back arrow, a circle for home, and a square to show open apps.
+        You can use these buttons to navigate between apps and return to the home screen.
+      </b-card-text>
+      </b-card>
+    <h4>How to find app</h4>
+    <b-card img-src="https://placekitten.com/300/300" img-alt="Card image" img-right>
+      <b-card-text>
+        On the home screen, look for an icon that looks like a square with dots inside. This is the "Apps" app.
+        Tap the “Apps” app to open it. Here you will find all installed apps on your phone. You can swipe up or down to scroll through them.
+      </b-card-text>
+      </b-card>
+    <h4>The home screen</h4>
+    <b-card img-src="https://placekitten.com/300/300" img-alt="Card image" img-left class="mb-3">
+      <b-card-text>
+       When you turn on the phone or press the home button, you will be taken to the home screen. It's where you'll find all your apps and widgets.
+       To return to the home screen when you're in another app, tap
+      </b-card-text>
+    </b-card>
+    <h4>How to customize wallpaper on your Android phone</h4>
+    <b-card img-src="https://placekitten.com/300/300" img-alt="Card image" img-right>
+      <b-card-text>
+        Touch and hold an empty space on the home screen until a menu appears.
+        Select "Background" or "Background image" from the menu.
+        Choose an image from the gallery or from preset wallpapers.  
+        Once you've selected your image, tap "Use as wallpaper" or similar to set it as your wallpaper.
+      </b-card-text>
+    </b-card>
+    <h4>Messages</h4>
+    <b-card img-src="https://placekitten.com/300/300" img-alt="Card image" img-right>
+      <b-card-text>
+        If you receive a message, a notice will appear at the top of the screen. You can also get a warning on the lock screen.
+        Swipe down from the top of the screen to open the notification bar and see all your messages.
+        Tap a message to open it and read it. You can reply by pressing the reply option.
+      </b-card-text>
+    </b-card>
+    <h4>The camera</h4>
+    <b-card img-src="https://placekitten.com/300/300" img-alt="Card image" img-left class="mb-3">
+      <b-card-text>
+        On the home screen, look for an icon that looks like a camera.
+        Tap the camera icon to open the camera app.
+        Now you can point and tap the screen to take a picture. There are also other settings and effects you can explore.
+      </b-card-text>
+    </b-card>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+// Detta är för komma längst upp på sidan man router- linka till.
+thods: {
+scrollToTop() {
+window.scrollTo(0, 0) // Scrolla till (x, y) = (0, 0)
+},
+},
+}
+</script>
+<style>
+
+</style>
