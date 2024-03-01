c9440c24236f4f7e6b5f6802a727e7853d369fad
DrKhaliil <khaliil.m.hassan.2003@gmail.com>
2024-03-01 10:04:27 +0100

    actual text to the pro
    readable text for ProCamera

--- src/views/Android/Stepbystep/ProCameraView.vue
+++ src/views/Android/Stepbystep/ProCameraView.vue
@@ -1,69 +1,164 @@
 <template>
     <!--KHALIIL HASSAN-->
-    <div class="text-width">
+    <div class="text-width" id="speech">
         <h1 class="title">Use your phone camera like a pro!</h1>
-        
-        <p class="p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam placeat temporibus alias ea rem cupiditate a optio nam facilis voluptatum quia doloribus ipsam voluptate, atque quae, repellendus reiciendis debitis. Cupiditate.</p>
 
-        <h1>lorem ipsum dolor</h1>
+        <p class="p">This article will be teaching you the basics of Pro camera.</p>
+
+        <h1 class="subheader">
+            Unlocking the Power of Pro Camera Settings on Your Android Phone
+        </h1>
         <div>
-            <img class="img" src="../../../assets/ProCam/procam1.webp" alt="pro">
+            <img class="img" src="../../../assets/ProCam/procam1.webp" alt="pro" />
         </div>
 
         <p class="p">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corrupti, quod officia alias inventore sit, excepturi sunt voluptatibus iusto eaque ratione, nihil nemo id repellendus et quos labore debitis nobis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint velit nobis dignissimos molestiae reprehenderit consequuntur exercitationem accusantium quia delectus unde enim, assumenda laboriosam non eligendi inventore esse. Hic, quaerat tempore. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam dolores delectus pariatur eum itaque, facilis soluta sit tempore nobis ad error quidem, dolor inventore dolorem sapiente minima ipsa officiis laborum?<br>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint velit nobis dignissimos molestiae reprehenderit consequuntur exercitationem accusantium quia delectus unde enim, assumenda laboriosam non eligendi inventore esse. Hic, quaerat tempore. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam dolores delectus pariatur eum itaque, facilis soluta sit tempore nobis ad error
+            <br />Step 1: Open the Camera App <br />
+
+            <br />Launch your camera app by tapping on the camera icon or accessing it
+            through the app drawer. Once open, locate the mode selector and find the
+            'Pro' or 'Manual' mode. It is usually represented by a gear or slider
+            icon. <br />
+
+            <br />Step 2: Adjusting ISO Settings <br />
+
+            <br />In Pro mode, you have the flexibility to adjust ISO settings. ISO
+            controls the sensitivity of your camera to light. A lower ISO is suitable
+            for well-lit environments, while a higher ISO is useful in low-light
+            conditions. Experiment with different ISO levels to achieve the desired
+            effect. <br />
+
+            <br />Step 3: Setting the Shutter Speed <br />
+
+            <br />Pro mode allows you to control the shutter speed, influencing the
+            amount of time the camera's sensor is exposed to light. A faster shutter
+            speed is ideal for capturing fast-moving subjects, while a slower speed
+            can create stunning long-exposure shots. Adjust the shutter speed
+            according to your creative vision. <br />
+
+            <br />Step 4: Tweaking White Balance <br />
+
+            <br />White balance ensures accurate color reproduction in your photos.
+            Pro mode lets you fine-tune the white balance settings based on the
+            lighting conditions. Experiment with presets like daylight, cloudy, or
+            tungsten, or manually adjust the color temperature for a personalized
+            touch. <br />
+
+            <br />Step 5: Manual Focus Control <br />
+
+            <br />Unlike auto-focus, Pro mode provides manual control over focus. Tap
+            on the screen to bring up the focus slider and adjust it to achieve the
+            desired focus point. This is particularly useful for creating a shallow
+            depth of field or capturing intricate details. <br />
+
+            <br />Step 6: Capturing RAW Images <br />
+
+            <br />For maximum editing flexibility, Pro mode often supports capturing
+            images in RAW format. Enable the RAW option in the camera settings to
+            preserve more image data, allowing for advanced post-processing.<br />
         </p>
-        <h1>lorem ipsum dolor</h1>
+        <h1 class="subheader">
+            Unleashing Pro Editing in Lightroom Mobile: A Step-by-Step Guide (extra)
+        </h1>
         <div>
-            <img class="img" src="../../../assets/ProCam/lightroom-mobile-editing-app.jpg" alt="lightroom">
+            <img class="img" src="../../../assets/ProCam/lightroom-mobile-editing-app.jpg" alt="lightroom" />
         </div>
-        
 
         <p class="p">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corrupti, quod officia alias inventore sit, excepturi sunt voluptatibus iusto eaque ratione, nihil nemo id repellendus et quos labore debitis nobis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint velit nobis dignissimos molestiae reprehenderit consequuntur exercitationem accusantium quia delectus unde enim, assumenda laboriosam non eligendi inventore esse. Hic, quaerat tempore. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam dolores delectus pariatur eum itaque, facilis soluta sit tempore nobis ad error quidem, dolor inventore dolorem sapiente minima ipsa officiis laborum?<br>
+            <br />Step 1: Launch Lightroom Mobile Open the Lightroom Mobile app on
+            your Android device. If you haven't already, download it from the Google
+            Play Store. Once opened, select the photo you want to edit or import a new
+            one. <br />
+
+            <br />Step 2: Accessing Pro Editing Tools Tap on the editing icon (usually
+            represented by sliders) to access the editing panel. Look for the 'Pro' or
+            'Manual' tab – it might be represented by a wrench icon. This is where
+            you'll find advanced editing controls for fine-tuning your photo.
+            <br />
+
+            <br />Step 3: Mastering Exposure and Contrast Adjust the exposure and
+            contrast sliders to control the overall brightness and contrast of your
+            photo. Fine-tune these settings to achieve the desired balance, ensuring
+            details are not lost in shadows or blown out in highlights. <br />
+
+            <br />Step 4: Perfecting Colors with White Balance Use the white balance
+            controls to correct or enhance the colors in your photo. You can manually
+            adjust the temperature and tint to achieve a natural look or apply
+            creative color grading for a unique style. <br />
+
+            <br />Step 5: Enhancing Details with Sharpness Navigate to the 'Details'
+            or 'Sharpness' tab to enhance the details in your photo. Adjust the
+            sharpness and noise reduction sliders to achieve the desired level of
+            clarity and reduce any unwanted noise in your image.
+            <br />
+
+            <br />Step 6: Creative Control with Split Toning Experiment with split
+            toning to add a creative touch to your photos. This feature allows you to
+            add different colors to the highlights and shadows, giving your images a
+            distinctive look. Play around with the sliders until you find the perfect
+            balance. <br />
+
+            <br />Step 7: Applying Lens Corrections Correct distortions and
+            imperfections in your photos with lens correction tools. Look for options
+            like 'Geometry' or 'Optics' to straighten lines, fix perspective, and
+            remove any lens-related distortions. <br />
+
+            <br />Step 8: Saving in High-Quality Formats Before saving your edited
+            masterpiece, ensure you're saving it in the highest quality. Lightroom
+            Mobile often allows you to save in formats like JPEG or DNG (Adobe's RAW
+            format). Choose the format that best suits your needs.<br />
         </p>
 
-        <h1> Lorem ipsum dolor </h1>
+        <h1 class="subheader">Recommended editing apps</h1>
         <div>
-            <img class="img" src="../../../assets/ProCam/editing-app.webp" alt="editing">
+            <img class="img" src="../../../assets/ProCam/editing-app.webp" alt="editing" />
         </div>
-        <p class="p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem officiis vitae nostrum pariatur assumenda error quo quam quae doloremque, debitis unde reprehenderit facere perspiciatis obcaecati vel totam atque ut. Suscipit.</p>
+        <p class="p">
+            <li>
+                <ul>Snapseed:</ul>
+                <ul></ul>
+            </li>
+        </p>
     </div>
     <div class="text-width">
         <p>written by: Khaliil Hassan</p>
     </div>
 </template>
 
-<script>
-</script>
+<script></script>
 
 <style scoped>
- .text-width{
-     width: 50%;
-     margin: 0 auto;
-     text-align: start;
-     font-weight: 600;
- }
+.text-width {
+    width: 80%;
+    margin: 0 auto;
+    text-align: start;
+    font-weight: 600;
+}
 
- .title{
-     text-align: center;
-     font-size: 40px;
-     font-weight: 700;
-     margin-bottom: 20px;
-     
- }
- .img{
-     width: 100%;
-     margin-bottom: 20px;
-     border-radius: 10px; 
-     box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-     border: 1px solid rgba(0, 0, 0, 0.2);
-     overflow: hidden;
- }
+.title {
+    text-align: center;
+    font-size: 40px;
+    font-weight: 700;
+    margin-bottom: 20px;
+}
 
- .p{
+.subheader {
+    font-size: 30px;
+    font-weight: 700;
+    margin-bottom: 20px;
+}
+
+.img {
+    width: 100%;
+    margin-bottom: 20px;
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+    border: 1px solid rgba(0, 0, 0, 0.2);
+    overflow: hidden;
+}
+
+.p {
     padding-bottom: 30px;
     border-bottom: solid;
- }
+}
 </style>
